0885b4db5a4d0b4f426494ea28f33182a5829242
代大大 <74844961+dai888-web@users.noreply.github.com>
2023-08-20 05:34:54 +0000

    1

--- main.py
+++ main.py
@@ -19,6 +19,11 @@ sk.listen(3)
 
 #接受客户端的数据，并返回两个参数，a为连接信息，b为客户端的ip地址与端口号
 
+def get_link(path):
+    for item in os.walk(path):
+        return item[0]
+    
+
 def get_dir(path):
     for item in os.walk(path):
         return item[1]
@@ -27,19 +32,15 @@ def get_dir_num(path):
     for item in os.walk(path):
         return len(item[1])
 
-def get_link(path):
-    for item in os.walk(path):
-        return item[0]
-    
-def produce(name,link):
+def produce_dir(name,link):
     main="<tr><td class='name'>"+name+"/</td><td class='size'>"+link+"</td>"+"<td class='more'>123</td></tr>"
     return main
 
-def merge_catalog(path):
+def merge_catalog_dir(path):
     main=""
     for i in range(0,get_dir_num(path),1):
-        print(produce(get_dir(path)[i],get_link(path)))
-        main+=produce(get_dir(path)[i],get_link(path))+"\n"
+        print(produce_dir(get_dir(path)[i],'\\'))
+        main+=produce_dir(get_dir(path)[i],'\\')+"\n"
     return main
 
 
@@ -47,49 +48,65 @@ def get_file(path):
     for item in os.walk(path):
         return item[2]
 
-print(get_file("./"))
+def get_file_num(path):
+    for item in os.walk(path):
+        return len(item[2])
+    
+def produce_file(name,link):
+    main="<tr><td class='name'>"+name+"</td><td class='size'>"+link+"</td>"+"<td class='more'>123</td></tr>"
+    return main
+
+def merge_catalog_file(path):
+    main=""
+    for i in range(0,get_file_num(path),1):
+        print(produce_file(get_file(path)[i],str(os.path.getsize(path))))
+        main+=produce_file(get_file(path)[i],str(os.path.getsize(path)))+"\n"
+    return main
+one=merge_catalog_dir("./")
+one+=merge_catalog_file("./")
 
 
-# while True:
-#     a,b=sk.accept()
-#     data=a.recv(1024)
-#     print(data.decode('utf-8'))
-#     print(a)
-#     message='''HTTP/1.0 200 OK
-# Content-Type: text/html
 
-# <html>
-#     <head>
-#         <meta charset="utf-8">
-#         <title>服务器文件</title>
-#     </head>
-#     <body>
-#         <h1>服务器文件管理系统</h1>
-#         <table class="table_file" border="1" cellspacing="0">
-#             <tr>
-#             <th >名字</th>
-#             <th >大小</th>
-#             <th >操作</th>
-#             </tr>
-#             <tr>
-#             <td class="name">123</td>
-#             <td class="size">123</td>
-#             <td class="more">123</td>
-#             </tr>'''+one+'''
-#         </table>
-#     </body>
-#     <style>
-#     .table_file{
-#     width:800px;
-#     }
-#     .table_file td {
-#     text-align: center;
-#     }
-# </style>
-# </html>'''
-#     # time.sleep(1)
-#     a.send(message.encode('utf-8'))
-#     break
+while True:
+    a,b=sk.accept()
+    data=a.recv(1024)
+    print(data.decode('utf-8'))
+    print(a)
+    message='''HTTP/1.0 200 OK
+Content-Type: text/html
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>服务器文件</title>
+    </head>
+    <body>
+        <h1>服务器文件管理系统</h1>
+        <table class="table_file" border="1" cellspacing="0">
+            <tr>
+            <th >名字</th>
+            <th >大小</th>
+            <th >操作</th>
+            </tr>
+            <tr>
+            <td class="name">123</td>
+            <td class="size">123</td>
+            <td class="more">123</td>
+            </tr>'''+one+'''
+        </table>
+    </body>
+    <style>
+    .table_file{
+    width:800px;
+    }
+    .table_file td {
+    text-align: center;
+    }
+</style>
+</html>'''
+    # time.sleep(1)
+    a.send(message.encode('utf-8'))
+    break
 
 
 
