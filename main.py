aa3c04c34b4bdbf04a2da528e3b440ab92dc8432
代大大 <74844961+dai888-web@users.noreply.github.com>
2023-08-20 05:59:40 +0000

    1

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import re
 #这是进行定义一个ip协议版本AF_INET（IPv4），定义一个传输TCP协议，SOCK_STREAM
 sk=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 #定义ip地址与端口号，ip地址就是服务端的ip地址，端口随便定义，但要与客户端脚本一致
-ip_port=("127.0.0.1",8000)
+ip_port=("127.0.0.1",8001)
 #绑定一个端口
 sk.bind(ip_port)
 #监听一个端口,这里的数字3是一个常量，表示阻塞3个连接，也就是最大等待数为3
@@ -53,14 +53,14 @@ def get_file_num(path):
         return len(item[2])
     
 def produce_file(name,link):
-    main="<tr><td class='name'>"+name+"</td><td class='size'>"+link+"</td>"+"<td class='more'>123</td></tr>"
+    main="<tr><td class='name'>"+name+"</td><td class='size'>"+link+"KB</td>"+"<td class='more'>123</td></tr>"
     return main
 
 def merge_catalog_file(path):
     main=""
     for i in range(0,get_file_num(path),1):
-        print(produce_file(get_file(path)[i],str(os.path.getsize(path))))
-        main+=produce_file(get_file(path)[i],str(os.path.getsize(path)))+"\n"
+        print(produce_file(get_file(path)[i],str(int(os.path.getsize(get_file(path)[i])/1024))))
+        main+=produce_file(get_file(path)[i],str(int(os.path.getsize(get_file(path)[i])/1024)))+"\n"
     return main
 one=merge_catalog_dir("./")
 one+=merge_catalog_file("./")
